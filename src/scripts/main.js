4bbf6330c2707e552e433c371ad66ba4083a2f38
CwaR228 <sosok2gt@gmail.com>
2025-07-31 19:09:02 +0300

    developed2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,20 @@
 'use strict';
 
-['thead', 'tbody', 'tfoot'].forEach(section => {
+['thead', 'tbody', 'tfoot'].forEach((section) => {
   const rows = document.querySelectorAll(`${section} tr`);
 
-  rows.forEach(row => {
+  rows.forEach((row) => {
     const cells = row.cells;
-    if (cells.length < 2) return;
+
+    if (cells.length < 2) {
+      return;
+    }
 
     const secondCell = cells[1];
     const lastCell = cells[cells.length - 1];
 
     const clonedCell = secondCell.cloneNode(true);
+
     row.insertBefore(clonedCell, lastCell);
   });
 });
